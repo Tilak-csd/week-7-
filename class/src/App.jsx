c20585b430c7c52bfeb981def8aabc1725a9f617
Tilak-csd <tilakgubhaju3@gmail.com>
2025-11-24 12:29:43 +0545

    updates

--- class/src/App.jsx
+++ class/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CountAtom } from './store/atom/count'
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <RecoilRoot>
+      <Count />
+    </RecoilRoot>
   )
 }
+function Count() {
+  return <div>
+    <CountRender />
+  </div>
+
+}
+
+function CountRender() {
+  const count = useRecoilValue(CountAtom)
+  return <div>
+    <p>Count is {count}</p>
+    <Button></Button>
+  </div>
+}
+
+function Button() {
+  const [count, setCount] = useRecoilState(CountAtom)
+  return <div>
+    <button onClick={() => setCount(count + 1)}>
+      Increase
+    </button>
+    <button onClick={() => setCount(count - 1)}> Decrease</button>
+  </div>
+} 
---
+++ class/src/store/atom/count.jsx
@@ -0,0 +1,5 @@
+import { atom } from 'recoil'
+export const CountAtom = atom({
+    key : "CountAtom",
+    default : 0
+})
